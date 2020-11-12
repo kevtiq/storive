49574cfa501e83df769d9a01d648092ec01dc016
Kevin Pennekamp <github@kevtiq.co>
2020-11-12 10:19:53 +0100

    made everything more strongly typed

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 export type State = Record<string, unknown>;
 export type Payload = Record<string, unknown>;
-export type Reducer = (state: State, payload?: Payload) => State | void;
-export type Query = (state: State) => State;
+export type Reducer<T> = (state: T, payload?: Payload) => T | void;
+export type Query<T> = (state: T) => T;
 export type Event = '@changed' | string;
-export type Store = {
-  get(query?: Query): State;
-  on(event: Event, reducer: Reducer): () => void;
+
+export type Store<T extends State> = {
+  get(query?: Query<T>): T;
+  on(event: Event, reducer: Reducer<T>): () => void;
   dispatch(event: Event, payload?: Payload): void;
   rollback(): void;
 };
@@ -15,14 +16,14 @@ function clone<T>(state: T): T {
   return JSON.parse(JSON.stringify(state));
 }
 
-export default function store(init: State): Store {
+export default function store<T extends State>(init: T): Store<T> {
   let _state = init;
-  const _reducers: Record<Event, Reducer[]> = { '@changed': [] };
-  const _log: [Event, State][] = [];
+  const _reducers: Record<Event, Reducer<T>[]> = { '@changed': [] };
+  const _log: [Event, T][] = [];
 
   return {
-    get: (query): State => {
-      return query ? query(clone<State>(_state)) : clone<State>(_state);
+    get: (query): T => {
+      return query ? query(clone<T>(_state)) : clone<T>(_state);
     },
     on: (event, reducer): (() => void) => {
       (_reducers[event] || (_reducers[event] = [])).push(reducer);
@@ -34,13 +35,13 @@ export default function store(init: State): Store {
     dispatch(event, payload): void {
       if (!_reducers[event]) return;
       // Set a copy of the new state on top of the event log.
-      _log.unshift([event, clone<State>(_state)]);
-      _reducers[event].forEach((r) => (_state = r(_state, payload) as State));
+      _log.unshift([event, clone<T>(_state)]);
+      _reducers[event].forEach((r) => (_state = r(_state, payload) as T));
       // Trigger all reducer on the store changes
       _reducers['@changed'].forEach((listener) => listener(_state));
     },
     rollback(): void {
-      _state = _log.shift()?.[1] || {};
+      _state = (_log.shift()?.[1] || {}) as T;
       _reducers['@changed'].forEach((listener) => listener(_state));
     },
   };
